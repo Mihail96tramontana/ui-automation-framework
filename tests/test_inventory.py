4b0294fc30b59bbec629402bc1149488aada73d6
Mikhail Liubeznow <znakomets2016@yandex.ru>
2026-07-12 19:50:15 +0300

    test: добавлены тесты по работе с продуктами каталога, изменена структура

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -23,7 +23,7 @@ class TestInventory:
         expect(page.locator('.title')).to_have_text('Products')
         expect(page.locator('.inventory_item')).to_have_count(6)
 
-        page.wait_for_timeout(3000)
+
 
     def test_adding_to_cart_single_item(self, page: Page):
 
@@ -43,23 +43,24 @@ class TestInventory:
         button_login.click()
 
         #добавляем товар в корзину
-        add_to_cart = page.locator('#add-to-cart-sauce-labs-backpack')
-        add_to_cart.click()
+        add_to_cart_one = page.locator('#add-to-cart-sauce-labs-backpack')
+        add_to_cart_one.click()
 
         #проверки
         expect(page).to_have_url(f'{BASE_URL}inventory.html') #проверка URL
         expect(page.locator('#remove-sauce-labs-backpack')).to_have_text('Remove') #проверка, что кнопка Add to cart поменялась на Remove
         expect(page.locator('.shopping_cart_badge')).to_have_text('1') #проверка, что каунтер товаров в корзине равен 1
 
-        #переходим в корзину
-        item_in_cart = page.locator('.shopping_cart_link')
-        item_in_cart.click()
+        #добавляем второй товар в корзину
+        add_to_cart_two = page.locator('#add-to-cart-sauce-labs-fleece-jacket')
+        add_to_cart_two.click()
 
-        #проверяем наличие добавленного товара в корзине
-        expect(page.locator('.inventory_item_name')).to_have_text('Sauce Labs Backpack')
+        #проверяем, что каунтер увеличился до 2
+        expect(page.locator('.shopping_cart_badge')).to_have_text('2')
 
 
-    def test_adding_to_cart_multiple_items(self, page: Page):
+
+    def test_removing_item_from_cart(self, page: Page):
 
         #переход на страницу
         page.goto(BASE_URL)
@@ -76,79 +77,83 @@ class TestInventory:
         button_login = page.locator('#login-button')
         button_login.click()
 
+        #добавляем товар в корзину
+        add_to_cart_one = page.locator('#add-to-cart-sauce-labs-backpack')
+        add_to_cart_one.click()
 
-        #добавляем товары в корзину
+        #добавляем второй товар в корзину
+        add_to_cart_two = page.locator('#add-to-cart-sauce-labs-fleece-jacket')
+        add_to_cart_two.click()
 
-        add_first_product = page.locator('#add-to-cart-sauce-labs-backpack')
-        add_first_product.click()
-        #проверяем, что каунтер корзины - 1
+        #удаляем первый товар из корзины
+        delete_item_one_from_cart = page.locator('#remove-sauce-labs-backpack')
+        delete_item_one_from_cart.click()
+        #проверяем, что каунтер уменьшился на 1
         expect(page.locator('.shopping_cart_badge')).to_have_text('1')
-        #проверяем, что кнопка изменилась с Add to cart на Remove
-        expect(page.locator('#remove-sauce-labs-backpack')).to_have_text('Remove')
-        #переходим в корзину
-        item_first_in_cart = page.locator('.shopping_cart_link')
-        item_first_in_cart.click()
-        #проверяем, что товар добавлен
-        expect(page.locator('#item_4_title_link .inventory_item_name')).to_have_text('Sauce Labs Backpack')
-        #возвращаемся на страницу списка товаров
-        page.go_back()
 
-        add_second_product = page.locator('#add-to-cart-sauce-labs-fleece-jacket')
-        add_second_product.click()
-        #проверяем, что каунтер корзины - 2
-        expect(page.locator('.shopping_cart_badge')).to_have_text('2')
-        #проверяем, что кнопка изменилась с Add to cart на Remove
-        expect(page.locator('#remove-sauce-labs-fleece-jacket')).to_have_text('Remove')
-        #переходим в корзину
-        item_first_in_cart = page.locator('.shopping_cart_link')
-        item_first_in_cart.click()
-        #проверяем, что товар добавлен
-        expect(page.locator('#item_5_title_link .inventory_item_name')).to_have_text('Sauce Labs Fleece Jacket')
-        #возвращаемся на страницу списка товаров
-        page.go_back()
-
-        add_third_product = page.locator('#add-to-cart-sauce-labs-onesie')
-        add_third_product.click()
-        #проверяем, что каунтер корзины - 3
-        expect(page.locator('.shopping_cart_badge')).to_have_text('3')
-        #проверяем, что кнопка изменилась с Add to cart на Remove
-        expect(page.locator('#remove-sauce-labs-onesie')).to_have_text('Remove')
-        #переходим в корзину
-        item_first_in_cart = page.locator('.shopping_cart_link')
-        item_first_in_cart.click()
-        #проверяем, что товар добавлен
-        expect(page.locator('#item_2_title_link .inventory_item_name')).to_have_text('Sauce Labs Onesie')
+        #удаляем второй товар из корзины
+        delete_item_two_from_cart = page.locator('#remove-sauce-labs-fleece-jacket')
+        delete_item_two_from_cart.click()
+        #проверяем, что каунтер исчез
+        expect(page.locator('.shopping_cart_badge')).to_be_hidden()
 
 
-        #итоговые проверки добавленных товаров в корзину
 
-        #проверяем, что всего 3 карточки товаров
-        expect(page.locator('.cart_item')).to_have_count(3)
+    def test_item_page(self, page: Page):
 
-        #проверяем, что цены соответствуют добавленным товарам
-        index_price_one = page.locator('.inventory_item_price').nth(0)
-        expect(index_price_one).to_have_text('$29.99')
+        #переход на страницу
+        page.goto(BASE_URL)
 
-        index_price_two = page.locator('.inventory_item_price').nth(1)
-        expect(index_price_two).to_have_text('$49.99')
+        #ввод логина
+        input_username = page.locator('#user-name')
+        input_username.fill(STANDARD_USER)
 
-        index_price_three = page.locator('.inventory_item_price').nth(2)
-        expect(index_price_three).to_have_text('$7.99')
+        #ввод пароля
+        input_password = page.locator('#password')
+        input_password.fill(PASSWORD)
 
-        #проверяем, что у всех товаров есть кнопки удаления из корзины - Remove
-        expect(page.locator('#remove-sauce-labs-backpack')).to_have_text('Remove')
-        expect(page.locator('#remove-sauce-labs-fleece-jacket')).to_have_text('Remove')
-        expect(page.locator('#remove-sauce-labs-onesie')).to_have_text('Remove')
+        #отправка формы/авторизация
+        button_login = page.locator('#login-button')
+        button_login.click()
 
-        #проверяем, что добавлены нужные товары
-        index_name_item_one = page.locator('.inventory_item_name').nth(0)
-        expect(index_name_item_one).to_have_text('Sauce Labs Backpack')
+        #проваливаемся в карточку товара
+        open_item = page.locator('img[inventory-item-sauce-labs-backpack-img]')
+        open_item.click()
 
-        index_name_item_two = page.locator('.inventory_item_name').nth(1)
-        expect(index_name_item_two).to_have_text('Sauce Labs Fleece Jacket')
+        #общие проверки страницы товара
+        expect(page).to_have_url(f'{BASE_URL}inventory-item.html?id=4')
+        expect(page.locator('.inventory_details_name large_size')).to_have_text('Sauce Labs Backpack')
+        expect(page.locator('.inventory_details_price')).to_have_text('$29.99')
+        expect(page.locator('.inventory_details_desc large_size')).to_have_text('carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.')
+        expect(page.locator('.btn btn_primary btn_small btn_inventory')).to_be_visible()
 
-        index_name_item_three = page.locator('.inventory_item_name').nth(2)
-        expect(index_name_item_three).to_have_text('Sauce Labs Onesie')
+        #добавляем товар в корзину
+        add_to_cart = page.locator('#add-to-cart')
+        add_to_cart.click()
+        #проверяем, что каунтер стал 1
+        expect(page.locator('.shopping_cart_badge')).to_have_text('1')
+
+        #удаляем товар из корзины
+        remove_item_from_cart = page.locator('#remove')
+        remove_item_from_cart.click()
+        #проверяем отсутствие каунтера у корзины
+        expect(page.locator('.shopping_cart_badge')).to_be_hidden()
+
+        #возвращаемся в каталог
+        go_back_to_catalog = page.locator('.back-to-products')
+        go_back_to_catalog.click()
+        #проверяем, что успешно вернулись назад
+        expect(page).to_have_url(f'{BASE_URL}inventory.html')
+
+
+
+
+
+
+
+
+
+
 
 
 
